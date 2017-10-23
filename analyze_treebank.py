e9b9c3cb868847504348f178d3ac3e61f6e78b4a
Andrey Kutuzov <akutuzov72@gmail.com>
2017-10-23 21:20:03 +0200

    Treebank analyzer

---
+++ analyze_treebank.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+import fileinput
+from itertools import permutations
+from collections import Counter
+import numpy as np
+
+
+def nonprojectivity(tree):
+    arcs = len(tree)
+    n_arcs = 0
+    nonprojectivesentence = False
+    pairs = permutations(tree, 2)
+    for pair in pairs:
+        (dep0, head0, token0, rel0) = pair[0]
+        (dep1, head1, token1, rel1) = pair[1]
+        if dep0 < dep1 < head0 < head1 or head1 < head0 < dep1 < dep0 or dep1 < head0 < head1 < dep0 \
+                or dep0 < head1 < head0 < dep1 or head0 < dep1 < dep0 < head1 or head1 < dep0 < dep1 < head0:
+            nonprojectivesentence = True
+            n_arcs += 2
+    nonprojective_ratio = n_arcs / arcs
+    return nonprojectivesentence, nonprojective_ratio
+
+
+relations = 'nsubj obj iobj csubj ccomp xcomp obl vocative expl dislocated advcl advmod discourse aux cop mark nmod ' \
+            'appos nummod acl amod det clf case conj cc fixed flat compound list parataxis orphan goeswith ' \
+            'reparandum punct root dep'.split()
+
+relations_counter = Counter({rel: 0 for rel in relations})  # Here will go probabilities of arc labels
+
+sentences = []
+
+current_sentence = []
+for line in fileinput.input():
+    if line.strip() == '':
+        sentences.append(current_sentence)
+        current_sentence = []
+        continue
+    if line.strip().startswith('#'):
+        continue
+    res = line.strip().split('\t')
+    (identifier, token, lemma, upos, xpos, feats, head, rel, misc1, misc2) = res
+    if '.' in identifier:
+        continue
+    current_sentence.append((float(identifier), float(head), token, rel))
+
+sentences.append(current_sentence)
+
+nonprojectivities = []
+non_arcs = []
+
+for i in range(len(sentences)):
+    sentence = sentences[i]
+    # print(' '.join([w[2] for w in sentence]), file=sys.stderr)
+    non_proj = nonprojectivity(sentence)
+    nonprojectivities.append(non_proj[0])
+    non_arcs.append(non_proj[1])
+    sent_relations = [w[3] for w in sentence]
+    rel_distribution = Counter({rel: sent_relations.count(rel) for rel in relations})
+
+    # Converting to probability distribution
+    total = sum(rel_distribution.values())
+    for key in rel_distribution:
+        rel_distribution[key] /= total
+
+    relations_counter = relations_counter + rel_distribution
+
+# Averaging probabilities of arc labels
+for key in relations_counter:
+    relations_counter[key] /= len(sentences)
+
+for rel in sorted(relations):
+    print(rel, relations_counter[rel])
+
+print('Non-projective sentences:', np.average(nonprojectivities))
+print('Non-projective arcs:', np.average(non_arcs))
